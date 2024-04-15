0055af846562e82497a434e161f27ff0f17e7049
Xittus <ewan.waldmann@gmail.com>
2024-04-15 13:18:51 +0200

    spring setup

---
+++ src/main/java/ch/m295/todorganizer/TodorganizerApplication.java
@@ -0,0 +1,13 @@
+package ch.m295.todorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodorganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/m295/todorganizer/TodorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.m295.todorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
